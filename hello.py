a556843e5f48d3448dfb779e8633f62dbfb8822f
EricIbanezH <89991725+EricIbanezH@users.noreply.github.com>
2021-09-21 10:40:09 -0500

    Create hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 import sys
 
 print('Hello, World!')
+
+
+print('The sum of 2 and 3 is 5.')
+
+
+sum = int(sys.argv[1]) + int(sys.argv[2])
+
+
+print('The sum of {0} and {1} is {2}.'.format(sys.argv[1], sys.argv[2], sum))
+
